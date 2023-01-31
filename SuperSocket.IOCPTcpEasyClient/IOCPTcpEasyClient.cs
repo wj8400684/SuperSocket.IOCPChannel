7496b44891985fc3e1c7bb0c1b14f68421cc83ea
吴俊 <8400684@qq.com>
2023-01-31 17:05:34 +0800

    删除重复代码

--- SuperSocket.IOCPTcpEasyClient/IOCPTcpEasyClient.cs
+++ SuperSocket.IOCPTcpEasyClient/IOCPTcpEasyClient.cs
@@ -7,11 +7,10 @@ using System.Net;
 
 namespace SuperSocket.IOCPEasyClient;
 
-public class IOCPTcpEasyClient<TPackage, TSendPackage> : EasyClient<TPackage>, IEasyClient<TPackage, TSendPackage>
+public class IOCPTcpEasyClient<TPackage, TSendPackage> : IOCPTcpEasyClient<TPackage>, IEasyClient<TPackage, TSendPackage>
     where TPackage : class
 {
     private IPackageEncoder<TSendPackage> _packageEncoder;
-    private IPipelineFilter<TPackage> _pipelineFilter;
 
     public IOCPTcpEasyClient(IPipelineFilter<TPackage> pipelineFilter, IPackageEncoder<TSendPackage> packageEncoder, ILogger? logger = null)
         : this(pipelineFilter, packageEncoder, new ChannelOptions { Logger = logger })
@@ -21,45 +20,12 @@ public class IOCPTcpEasyClient<TPackage, TSendPackage> : EasyClient<TPackage>, I
     public IOCPTcpEasyClient(IPipelineFilter<TPackage> pipelineFilter, IPackageEncoder<TSendPackage> packageEncoder, ChannelOptions options)
         : base(pipelineFilter, options)
     {
-        _pipelineFilter = pipelineFilter;
         _packageEncoder = packageEncoder;
     }
 
-    protected override async ValueTask<bool> ConnectAsync(EndPoint remoteEndPoint, CancellationToken cancellationToken)
+    public virtual ValueTask SendAsync(TSendPackage package)
     {
-        var connector = GetConnector();
-        var state = await connector.ConnectAsync(remoteEndPoint, null, cancellationToken);
-
-        if (state.Cancelled || cancellationToken.IsCancellationRequested)
-        {
-            OnError($"The connection to {remoteEndPoint} was cancelled.", state.Exception);
-            return false;
-        }
-
-        if (!state.Result)
-        {
-            OnError($"Failed to connect to {remoteEndPoint}", state.Exception);
-            return false;
-        }
-
-        var socket = state.Socket;
-
-        if (socket == null)
-            throw new Exception("Socket is null.");
-
-        var channelOptions = Options;
-        SetupChannel(new IOCPTcpPipeChannel<TPackage>(socket, _pipelineFilter, channelOptions));
-        return true;
-    }
-
-    public virtual async ValueTask SendAsync(TSendPackage package)
-    {
-        await SendAsync(_packageEncoder, package);
-    }
-
-    public new IEasyClient<TPackage, TSendPackage> AsClient()
-    {
-        return this;
+        return SendAsync(_packageEncoder, package);
     }
 }
 
